f5ab16900b525c988f4cedcf17010b8d45c43546
Bagas Dewanusa <aryasenabagas.work@gmail.com>
2024-09-29 08:54:25 +0700

    teleMessage on bob too (fix)

---
+++ app/api/dailySum/route.ts
@@ -0,0 +1,8 @@
+// To stream responses you must use Route Handlers in the App Router, even if the rest of your app uses the Pages Router.
+import { sendTele } from '@/app/lib/action'
+
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+export function GET(request: Request) {
+    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
